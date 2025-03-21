9ea8f7de418671e35d8cacab970bfa317d39f4ef
aramrw <106574385+aramrw@users.noreply.github.com>
2025-03-20 23:37:00 -0700

    debug

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
 use chrono::{DateTime, Local};
 use color_print::cprintln;
 
+/// Debug Date-Time
+macro_rules! dbug {
+    ($s:expr) => {{
+        let dt: DateTime<Local> = Local::now();
+        cprintln!("{} <b>DEBUG</>: {}", dt, $s);
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn tplt() {
+        dbug!("hello world");
+    }
+}
