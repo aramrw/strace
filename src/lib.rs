efa9333c7296a69d5a16ae723a77ff9da2239790
aramrw <106574385+aramrw@users.noreply.github.com>
2025-03-21 14:58:21 -0700

    add error macro

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,9 @@ mod macros {
     /// # Example
     /// ```
     /// use strace::dbug;
+    ///
+    /// // 2025-03-21 14:52:40.011389900 -07:00 DEBUG: serving...
     /// dbug!("serving...")
-    /// // output:
     /// ```
     #[macro_export]
     macro_rules! dbug {
@@ -21,7 +22,34 @@ mod macros {
         }};
         ($msg:expr, $title:expr) => {{
             let dt: $crate::chrono::DateTime<$crate::chrono::Local> = $crate::chrono::Local::now();
-            $crate::color_print::cprintln!("{} <b>DEBUG</> _ <bold>{}</bold>: {}", dt, $title, $msg);
+            $crate::color_print::cprintln!("{} <b>DEBUG</> <bold>{}</bold>: {}", dt, $title, $msg);
+        }};
+    }
+
+    /// Error Date-Time
+    ///
+    /// # Example
+    /// ```
+    /// use strace::err;
+    ///
+    /// // 2025-03-21 14:52:40.011389900 -07:00 ERROR: serving...
+    /// err!("fatal...")
+    /// // output:
+    /// ```
+    #[macro_export]
+    macro_rules! err {
+        ($msg:expr) => {{
+            let dt: $crate::chrono::DateTime<$crate::chrono::Local> = $crate::chrono::Local::now();
+            $crate::color_print::cprintln!("{} <r>ERROR</>: {:#?}", dt, $msg);
+        }};
+        ($msg:expr, $title:expr) => {{
+            let dt: $crate::chrono::DateTime<$crate::chrono::Local> = $crate::chrono::Local::now();
+            $crate::color_print::cprintln!(
+                "{} <r>ERROR</> <bold>{}</bold>: {:#?}",
+                dt,
+                $title,
+                $msg
+            );
         }};
     }
 }
@@ -30,7 +58,11 @@ mod macros {
 mod tests {
     use super::*;
     #[test]
-    fn tplt() {
+    fn tdbug() {
         dbug!("hello world");
+    }
+    #[test]
+    fn terr() {
+        err!("io error #53", "fatal");
     }
 }
